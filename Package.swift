27cb0f81004889d006f6abf7d4c4e4e05ffdfc00
nemanja-simic <nemanja.simic@sotech.si>
2024-11-20 10:35:21 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "CrowdinSDK", targets: ["CrowdinSDK"])
     ],
     dependencies: [
-        .package(url: "https://github.com/serhii-londar/BaseAPI.git", .upToNextMajor(from: "0.2.2")),
+        .package(url: "https://github.com/nemanja-simic/BaseAPI", .upToNextMajor(from: "master")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.8"))
     ],
     targets: [
